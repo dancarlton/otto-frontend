b1b4e5bf1b0086dc6a9541539d34f6c510ce752c
dancarlton <dan@dancarlton.com>
2025-06-30 11:46:15 -0700

    updated signup modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,28 @@
-import SearchBar from "../SearchBar/SearchBar";
-import "./Header.css";
-import logo from "/src/assets/logo.png";
+import SearchBar from '../SearchBar/SearchBar'
+import './Header.css'
+import logo from '/src/assets/logo.png'
 
 export default function Header({ onLoginClick, onRegisterClick }) {
   return (
-    <header className="header">
-      <div className="header__container">
-        <div className="header__logo">
-          <img src={logo} alt="Otto logo" />
+    <header className='header'>
+      <div className='header__container'>
+        <div className='header__logo'>
+          <img src={logo} alt='Otto logo' />
           <span>OTTO</span>
         </div>
+        <div className='header__search'>
+          <SearchBar />
+        </div>
 
-        <SearchBar />
-
-        <div className="header__auth">
-          <button onClick={onLoginClick} className="button-outline">
+        <div className='header__auth'>
+          <button onClick={onLoginClick} className='button-outline'>
             Log in
           </button>
-          <button onClick={onRegisterClick} className="button-filled">
+          <button onClick={onRegisterClick} className='button-filled'>
             Create an account
           </button>
         </div>
       </div>
     </header>
-  );
+  )
 }
