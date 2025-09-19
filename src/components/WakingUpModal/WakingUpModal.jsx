3fab90ca1013049b9aa493b74de8804d4b5a38c0
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-18 23:09:22 -0700

    updated css to only change WakingUpModal

--- src/components/WakingUpModal/WakingUpModal.jsx
+++ src/components/WakingUpModal/WakingUpModal.jsx
@@ -10,8 +10,9 @@ function WakingUpModal({ isOpen, onClose }) {
       isOpen={isOpen}
       onClose={onClose}
       buttonText='Close'
+      className='waking-up'
     >
-      <div className='modal__content'>
+      <div className='modal__content--spinner'>
         <Lottie animationData={spinner} />
         <p>🐾 Servers are fetching… Otto’s still paddling.</p>
         <p>Grab a towel — we’ll be ready in just a moment.</p>
